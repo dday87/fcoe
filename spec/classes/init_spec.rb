d00ac8b57c53496be314579f3607d9a5170b0740
stivesso <stivesso@gmail.com>
2016-11-19 07:20:31 +0100

    Update init_spec.rb

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,10 +1,25 @@
 require 'spec_helper'
 describe 'fcoe' do
 
-  let :facts do
-    { :os => { 'family' => 'RedHat' } }
+  let :default_params do
+    {
+      :fcoe_packages     => '["fcoe-utils","lldpad"]',
+      :fcoe_service      => '["fcoe","lldpad"]',
+      :network_service   => 'network',
+    }
   end
-  context 'with defaults for all parameters' do
-    it { should contain_class('fcoe') }
+  describe 'os-dependent items' do
+    context "on RedHat based systems" do
+      let :default_facts do
+        {
+          :os                     => { :family => 'RedHat', :release => { :major => '7', :minor => '1', :full => '7.1.1503' } },
+          :kernel                 => 'Linux',
+          :path                   => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+        }
+      end
+      let :params do default_params end
+      let :facts do default_facts end
+      it { is_expected.to contain_class("fcoe") }
+    end
   end
 end
